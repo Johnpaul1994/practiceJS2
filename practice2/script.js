edef84826b44644c898ef3d172af063cb62cb3aa
Johnpaul1994 <johnpaulnedolisa@gmail.com>
2023-05-02 13:22:20 +0100

    multiplication excercise

---
+++ practice2/script.js
@@ -0,0 +1,37 @@
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+
+const questionEl = document.getElementById("question");
+
+const inputEl = document.getElementById("input")
+
+const formEl = document.getElementById("form")
+
+questionEl.innerText = `what is ${num1} multiply by ${num2}`
+
+let score = JSON.parse(localStorage.getItem("getItem"));
+
+if (!score) {
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`
+
+const scoreEl = document.getElementById("score")
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () =>{
+    const userAns = +inputEl.value
+    if (userAns === correctAns) {
+        score++;
+        updateLocalStorage()
+    }else {
+        score--;
+        updateLocalStorage()
+    }
+});
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringyfy(score))
+}
